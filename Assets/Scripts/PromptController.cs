cc68bec649af67010045c4f8beb2b59cf82146ab
Daniel <65472342+DVK02@users.noreply.github.com>
2023-09-26 19:42:45 -0400

    milestone1 build

---
+++ Assets/Scripts/PromptController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromptController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+}
---
+++ Assets/Scripts/ValidatePhoto.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ValidatePhoto : MonoBehaviour
+{
+
+    public const float ACCEPT_POS_DEVIATION = 5;
+    public const float ACCEPT_ANGLE_DEVIATION = 10;
+    private new GameObject camera;
+
+
+
+    void Start()
+    {
+        camera = GameObject.FindGameObjectsWithTag("1st person camera")[0];
+    }
+
+    private bool IsAttemptAcceptable(Vector3 attemptPos, Vector3 attemptAngle)
+    {
+        float posDeviation = (attemptPos - GameManager.Instance.GetPositionSolution()).magnitude;
+        float angDeviation = angleDeviation(attemptAngle, GameManager.Instance.GetAngleSolution()).magnitude;
+        return (posDeviation <= ACCEPT_POS_DEVIATION) && (angDeviation <= ACCEPT_ANGLE_DEVIATION);
+    }
+
+    private Vector3 angleDeviation(Vector3 va, Vector3 vb)
+    {
+        return new Vector3(
+          Mathf.DeltaAngle(va.x, vb.x),
+          Mathf.DeltaAngle(va.y, vb.y),
+          Mathf.DeltaAngle(va.z, vb.z)
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            Vector3 attemptPos = camera.transform.position;
+            Vector3 attemptAngle = camera.transform.eulerAngles;
+
+            Debug.Log(string.Format("POS: {0} ANG: {1}", attemptPos, attemptAngle));
+
+            if (IsAttemptAcceptable(attemptPos, attemptAngle))
+            {
+                Debug.Log("REWARD");
+                GameManager.Instance.UpdateStage();
+            }
+            else
+            {
+                Debug.Log("NO REWARD");
+            }
+        }
+    }
+}
