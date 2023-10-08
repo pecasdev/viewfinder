56f5be851f03c3e5356fa201997bd46e7e882432
Alicia <aliciagnatividad@gmail.com>
2023-10-08 17:37:11 -0400

    Base object interaction in level demo

---
+++ Assets/Scripts/PickUpController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+interface IObjectToPickUp
+{
+    public void PickUp(Transform holdArea);
+    public void PutDown();
+
+    public void Move(Transform holdArea, float pickUpForce);
+}
+
+// Attach to player camera
+public class PickUpController : MonoBehaviour
+{
+    [SerializeField] private Transform _holdArea;
+    [SerializeField] private float _pickupRange = 5.0f;
+    [SerializeField] private float _pickupForce = 150.0f;
+    private IObjectToPickUp heldObj;
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)){
+            Debug.Log("E");
+            if (heldObj == null)
+            {
+                Ray r = new Ray(transform.position, transform.forward);
+                if (Physics.Raycast(r, out RaycastHit hitInfo, _pickupRange))
+                {
+                    Debug.Log("Raycast");
+                    if (hitInfo.collider.gameObject.TryGetComponent(out IObjectToPickUp pickObj))
+                    {
+                        Debug.Log("hit an interactable");
+                        heldObj = pickObj;
+                        pickObj.PickUp(_holdArea);
+                    }
+                }
+            } else
+            {
+                
+                heldObj.PutDown();
+                heldObj = null;
+
+            }
+        }
+
+        if (heldObj != null)
+        {
+            heldObj.Move(_holdArea, _pickupForce);
+        }
+        
+    }
+}
