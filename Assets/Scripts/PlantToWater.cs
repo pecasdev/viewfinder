5d1010abb7f125c103cfa66dedee6ffa8b59b5f1
Alicia <aliciagnatividad@gmail.com>
2023-10-15 18:57:49 -0400

    Set up level 3 room and watering script

---
+++ Assets/Scripts/PlantToWater.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantToWater : MonoBehaviour, IPlantToWater
+{
+    bool _isWilted = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Bloom()
+    {
+        // play bloom animation
+        if (_isWilted)
+        {
+            // play bloom animation
+            _isWilted = false;
+        }
+    }
+}
