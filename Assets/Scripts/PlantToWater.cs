305d150a7c394d3fe01a584a6516be9bf2e76b72
Alicia <aliciagnatividad@gmail.com>
2023-10-17 14:18:29 -0400

    Sample Shelf and pot plant mechanics

--- Assets/Scripts/PlantToWater.cs
+++ Assets/Scripts/PlantToWater.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlantToWater : MonoBehaviour, IPlantToWater
 {
     private bool _isWilted = true;
+    private Renderer _plantRenderer;
+    [SerializeField] private Animator _plantAnimator;
+    [SerializeField] private Color _wiltedColor = new Color(145f, 123f, 26f, 1f);
+    [SerializeField] private Color _bloomColor = new Color(89f, 181f, 56f, 1f);
+    [SerializeField] private float _bloomingSeconds = 2f;
+    private float _targetPoint = 0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _plantRenderer = GetComponent<Renderer>();
+        _plantRenderer.material.SetColor("_Color", _wiltedColor);
+        if (transform.GetComponent<Animator>())
+        {
+            _plantAnimator = transform.GetComponent<Animator>();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_isWilted == false && _plantRenderer.material.color != _bloomColor)
+        {
+            ColourTransition();
+        }
     }
 
     public void Bloom()
@@ -23,12 +38,16 @@ public class PlantToWater : MonoBehaviour, IPlantToWater
         // play bloom animation
         if (_isWilted)
         {
-            // play bloom animation
+            Debug.Log("Bloom!");
+            _plantAnimator.SetTrigger("Bloom");
+           
             _isWilted = false;
-            if (transform.GetComponent<Animator>())
-            {
-                transform.GetComponent<Animator>().SetTrigger("Bloom");
-            }
         }
+    }
+
+    private void ColourTransition()
+    {
+        _targetPoint += Time.deltaTime / _bloomingSeconds;
+        _plantRenderer.material.color = Color.Lerp(_wiltedColor, _bloomColor, _targetPoint);
     }
 }
