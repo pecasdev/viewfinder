6bda699b9ff4bf4b026837b3e168c0135a864ca3
Peter Albu <pecasdev@gmail.com>
2023-10-31 18:10:41 -0400

    Remove debug logging

--- Assets/Scripts/ResetPositionIfOOB.cs
+++ Assets/Scripts/ResetPositionIfOOB.cs
@@ -19,15 +19,6 @@ public class ResetPositionIfOOB : MonoBehaviour
         if (!bounds.Contains(transform.position))
         {
             transform.position = originalPosition;
-            Debug.Log("moving");
-        }
-        else
-        {
-            Debug.Log("not moving");
         }
     }
 }
-
-// Bounds(-14, 21, 4.69999981, 1, 10, 1.39999998)
-
-//UnityEditor.TransformWorldPlacementJSON:{"position":{"x":6.300000190734863,"y":3.2100000381469728,"z":4.699999809265137},"rotation":{"x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{"x":27.729999542236329,"y":10.0,"z":29.889999389648439}}
